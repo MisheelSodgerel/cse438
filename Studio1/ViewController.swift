b5f790c328e3304cc9492ba75d2e7b9d0e886477
MisheelSodgerel <misheel@wustl.edu>
2022-06-18 01:44:00 +0200

    Initial Commit

---
+++ Studio1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Studio1
+//
+//  Created by Misheel Sodgerel on 6/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
