1ef09be9fc8d520cf352d072037a526d14555102
MisheelSodgerel <misheel@wustl.edu>
2022-06-22 22:19:32 -0500

    Initial Commit

---
+++ Lab-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab-1
+//
+//  Created by Misheel Sodgerel on 6/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
