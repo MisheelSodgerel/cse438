5c43a67a1c406d93e9c49fed63e92091579536b5
MisheelSodgerel <misheel@wustl.edu>
2022-06-23 20:32:18 -0500

    Label not updating live.

--- Lab-1/ViewController.swift
+++ Lab-1/ViewController.swift
@@ -7,8 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var original: UITextField!
+    @IBOutlet weak var discount: UITextField!
+    @IBOutlet weak var sales: UITextField!
+    
+    @IBOutlet weak var finalPrice: UILabel!
+    
+    @IBAction func originalAction(_ sender: UILabel) {
+        let originalPrice = Double(original.text!)
+        let discountPercent = Double(discount.text!)
+        let salesPercent = Double(sales.text!)
+        
+        let afterDiscount = originalPrice! - (originalPrice! * discountPercent! * 0.01)
+        let final = afterDiscount + (afterDiscount * salesPercent! * 0.01)
+        
+        finalPrice.text = "$\(final)"
+    }
+    @IBAction func discountAction(_ sender: UILabel) {
+        let originalPrice = Double(original.text!);
+        let discountPercent = Double(discount.text!);
+        let salesPercent = Double(sales.text!);
+        
+        let afterDiscount = originalPrice! - (originalPrice! * discountPercent! * 0.01)
+        let final = afterDiscount + (afterDiscount * salesPercent! * 0.01)
+        
+        finalPrice.text = "$\(final)"
+    }
+    @IBAction func salesAction(_ sender: UILabel) {
+        let originalPrice = Double(original.text!);
+        let discountPercent = Double(discount.text!);
+        let salesPercent = Double(sales.text!);
+    
+        let afterDiscount = originalPrice! - (originalPrice! * discountPercent! * 0.01)
+        let final = afterDiscount + (afterDiscount * salesPercent! * 0.01)
+        
+        finalPrice.text = "$\(final)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
