343d1a2ab9237aeed33ea10149406388ec671711
MisheelSodgerel <misheel@wustl.edu>
2022-06-29 20:13:00 -0500

    Initial Commit

---
+++ Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by Misheel Sodgerel on 6/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
