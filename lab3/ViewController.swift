95c4ecbaacfe53deaaedfecd28a0bfa23119ba0a
MisheelSodgerel <misheel@wustl.edu>
2022-07-07 20:38:37 -0500

    storyboard layout is perfect. still need to add code

--- lab3/ViewController.swift
+++ lab3/ViewController.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func  touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard touches.count == 1,
+        let touchPoint = touches.first?.location(in: view)
+        else { return }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard touches.count == 1,
+         let touchPoint = touches.first?.location(in: view)
+        else { return }
+    }
+    
+    override func  touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard touches.count == 1,
+         let touchPoint = touches.first?.location(in: view)
+        else { return }
+    }
+    
 
 
 }
